d79cde99221e1761a99600e74b85579f4d39b748
jiaqi <1140103268@qq.com>
2017-06-04 10:03:06 +0800

    stock

--- stock system/src/app/stock/stock-form/stock-form.component.ts
+++ stock system/src/app/stock/stock-form/stock-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Stock } from '../stock.service';
-import { ActivatedRoute } from '';
+import { ActivatedRoute } from '@angular/router';
 import { StockService } from '../stock.service';
 
 
@@ -13,10 +13,10 @@ export class StockFormComponent implements OnInit {
 
   stock:Stock;
 
-  constructor(private routeInfo:ActivatedRoute, private stockService:StockService) { }
+  constructor(private routeInfo: ActivatedRoute, private stockService:StockService) { }
 
   ngOnInit() {
-    this.stock = new Stock(1, "第一只股票", 1.99, 3.5, "这是第一只股票,Angular2实战股票", ["IT", "互联网"]);
+    this.stock = new Stock(1, '第一只股票', 1.99, 3.5, '这是第一只股票,Angular2实战股票', ['IT', '互联网']);
   }
 
 }
